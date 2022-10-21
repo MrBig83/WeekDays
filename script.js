be617e5bd3345172b5e48fcf1ff9e64ae6172e04
MrBig83 <mjohansson176@gmail.com>
2022-10-21 18:32:58 +0200

    Testing functions

--- script.js
+++ script.js
@@ -3,13 +3,22 @@ let endDate = document.getElementById("end")
 
 let btn = document.getElementById("btn")
 
-let difference = startDate - endDate
+//let difference = startDate - endDate
+
+let sDate = Date(startDate.value)
+let eDate = Date(endDate.value)
+let testDate = new Date(10102022)
 
 console.log(startDate)
 
 btn.addEventListener("click", function(){
-    console.log("click")
+    
     console.log(startDate.value)
     console.log(endDate.value)
-    console.log(difference)
+    //console.log(difference)
+    console.log(sDate)
+    console.log(eDate)
+    console.log(testDate)
+
+    console.log(typeof(startDate))
 })
